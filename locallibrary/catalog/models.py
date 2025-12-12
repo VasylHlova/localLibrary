796a39fcba04f9cf4107802365e47753d2462b49
vasyl <vasylhlova24@gmail.com>
2025-12-12 16:43:32 +0200

    Added home page and list of books page

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -59,7 +59,7 @@ class Book(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('bool-detail', args=[str(self.id)])
+        return reverse('book-detail', args=[str(self.id)])
     
 class BookInstance(models.Model):
     LOAN_STATUS = (
