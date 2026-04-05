c096d4f3fd54637f26e3e7e42ea81628270102f9
vasyl <vasylhlova24@gmail.com>
2026-04-06 00:45:39 +0300

    feat: add daily automatic changing status on available for books with overdue reservation; mailing system using celery: global daily task which gather all book with expiring loan and reservetion date then for matched calls task wich create and send notification.
    refactor: rewrite signals that deleted old images of author and book. So now this signals just calls the corresponding task which does all work.

---
+++ locallibrary/catalog/tasks/notification_tasks.py
@@ -0,0 +1,69 @@
+import smtplib
+from celery import shared_task
+
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.html import strip_tags
+from django.urls import reverse
+from django.conf import settings
+
+from ..models import BookInstance 
+from utils.choices import InstanceStatus
+
+@shared_task(
+        name='catalog.send_return_reminder_email', 
+        bind=True,
+        autoretry_for=(smtplib.SMTPException, TimeoutError, ConnectionRefusedError),
+        retry_kwargs={'max_retries': 3},
+        retry_backoff=True, 
+        retry_jitter=True
+)
+def send_return_reminder_email(self, book_instance_id: str): 
+    try:
+        book_instance = BookInstance.objects.select_related('book', 'borrower').get(id=book_instance_id)
+    except BookInstance.DoesNotExist:
+        return "BookInstance not found."
+
+    if book_instance.status not in [InstanceStatus.ON_LOAN, InstanceStatus.RESERVED]:
+        return f"Book {book_instance_id} is not on loan. Email aborted."
+        
+    if not book_instance.borrower or not book_instance.borrower.email:
+         return f"No borrower or email for instance {book_instance_id}."
+
+    today = timezone.now().date()
+    days_left = (book_instance.due_back - today).days
+
+    if days_left < 0:
+        days_text = "The deadline has already passed!"
+    else:
+        days_text = f"Days left: {days_left}"
+
+    domain = getattr(settings, 'SITE_URL', 'http://127.0.0.1:8000')
+    
+    book_url = f"{domain}{reverse('book-detail', kwargs={'pk': str(book_instance.book.pk)})}"
+    book_title = book_instance.book.title
+    book_status = 'loan' if book_instance.status == InstanceStatus.ON_LOAN else 'reservation'
+    
+    receiver_email = book_instance.borrower.email 
+
+    html_message = f"""
+    <html>
+        <body>
+            <h3>Book Return Reminder</h3>
+            <p>Hello {book_instance.borrower.first_name},</p>
+            <p>Your {book_status} deadline for the book <a href="{book_url}"><b>{book_title}</b></a> is expiring soon.</p>
+            <p style="color: red; font-weight: bold;">{days_text}</p>
+        </body>
+    </html>
+    """
+
+    plain_message = strip_tags(html_message)
+
+    send_mail(
+        subject=f'Your {book_status} period for "{book_title}" is expiring soon!',
+        message=plain_message,          
+        html_message=html_message,     
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[receiver_email],
+        fail_silently=False,            
+    )
